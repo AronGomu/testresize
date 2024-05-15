a77eaa0b993b2d148b8e11a7fdc3278eddd11abb
AronGomu <neverismine@gmail.com>
2024-05-15 13:04:27 +0200

    add some code

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const { when } = require("jquery")
-
 var Blog = Backbone.Model.extend({
 	defaults: {
 		author: "",
@@ -21,30 +19,58 @@ var blogs = new Blogs(blogList)
 
 console.log(blogs.toJSON())
 
-var BlogView = Backbone.View.extend({
-	model: new logs,
-	tagName: 'tr',
-	initialize: function() {
-		this.template = _.template(
-			$('blogs-list-template').html()
-		)
-	},
-	render: function() {
-		this.$el.html(this.template({this.model.toJSON()}))
-	}
-})
+// var BlogView = Backbone.View.extend({
+// 	model: new logs,
+// 	tagName: 'tr',
+// 	initialize: function() {
+// 		this.template = _.template(
+// 			$('blogs-list-template').html()
+// 		)
+// 	},
+// 	render: function() {
+// 		this.$el.html(
+// 			this.template(
+// 				{this.model.toJSON()}
+// 			)
+// 		)
+// 	}
+// })
+//
+// var BlogsView = Backbone.View.extend({
+// 	model: blogs,
+// 	el: $('.blogs-list'),
+// 	initialize: function({
+// 		this.model.on('add', this.render(), this)
+// 	}),
+// 	render: function() {
+// 		var self = this
+// 		this.$el.html('')
+// 		_.each(this.model.toArray(), function(blog) {
+// 			self.$el.append((new BlogView({model:blog})).render().$el);
+// 		})
+// 	}
+// })
+//
 
-var BlogsView = Backbone.View.extend({
-	model: blogs,
-	el: $('.blogs-list'),
-	initialize: function({
-		this.model.on('add',this.render(), this);
-	}),
-	render: function() {
-		var self = this;
-		this.$el.html('');
-		_.each(this.model.toArray(), function(blog) {
-			self.$el.append((new BlogView({model:blog})).render().$el);
-		})
-	}
-})
+var backgrid = new Backgrid.extend
+
+
+// Add sizeable columns
+var sizeAbleCol = new Backgrid.Extension.sizeAbleColumns({
+  collection: blogs,
+  columns: columns
+});
+$backgridContainer.find('thead').before(sizeAbleCol.render().el);
+ 
+// Add resize handlers
+var sizeHandler = new Backgrid.Extension.sizeAbleColumnsHandlers({
+  sizeAbleColumns: sizeAbleCol,
+  grid: pageableGrid,
+  saveModelWidth: true
+});
+$backgridContainer.find('thead').before(sizeHandler.render().el);
+ 
+// Listen to resize events
+columns.on('resize', function(columnModel, newWidth, oldWidth) {
+  console.log('Resize event on column; name, model, new and old width: ', columnModel.get("name"), columnModel, newWidth, oldWidth);
+});
